e07af3b98966f130ad1cd4693434127c1d48b0df
WillMS28 <silva.miranda.william@gmail.com>
2021-06-06 14:59:45 -0300

    Global Style

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global'
+
 function App() {
   return (
-    <h1>Hello world</h1>
+    <>
+      <h1>Hello world</h1>
+      <GlobalStyle />
+    </>
   );
 }
 
