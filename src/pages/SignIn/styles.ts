44305f2b376db04b8fd995a6cd0e904cbe247c31
WillMS28 <silva.miranda.william@gmail.com>
2021-06-19 21:54:41 -0300

    animated Sign and SignUp pages

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,4 +1,4 @@
-import styled  from 'styled-components'
+import styled, { keyframes }  from 'styled-components'
 import { shade } from 'polished'
 
 import signInbackgroundImg from '../../assets/sign-in-background.png'
@@ -8,6 +8,8 @@ export const Container = styled.div`
 
   display: flex;
   align-items: stretch;
+
+  overflow-x: hidden;
 `
 export const Content = styled.div`
   display: flex;
@@ -19,50 +21,90 @@ export const Content = styled.div`
   width: 100%;
   max-width: 700px;
 
-  h1 {
-    margin-bottom: 24px;
+`
+
+const appearFromLeft = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX( -50px )
   }
-
-  form {
-    margin: 80px 0;
-    width: 340px;
-    text-align: center;
-  }
-
-  a {
-    color: #f4ede8;
-    display: block;
-    margin-top: 24px;
-    text-decoration: none;
-    transition: color 0.2s;
-
-    &:hover {
-      color: ${shade(0.2, '#f4ede8')};
-    }
-  }
-
-  > a {
-    color: #ff9000;
-    display: block;
-    margin-top: 24px;
-    text-decoration: none;
-    transition: color 0.2s;
-
-    display: flex;
-    align-items: center;
-
-    svg {
-      margin-right: 16px;
-    }
-
-    &:hover {
-      color: ${shade(0.2, '#ff9000')};
-    }
-
+  to {
+    opacity: 1;
+    transform: translateX( 0 )
   }
 `
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  place-content: center;
+
+  width: 100%;
+  max-width: 700px;
+
+  animation: ${appearFromLeft} 1.5s;
+
+  h1 {
+      margin-bottom: 24px;
+    }
+
+    form {
+      margin: 80px 0;
+      width: 340px;
+      text-align: center;
+    }
+
+    a {
+      color: #f4ede8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#f4ede8')};
+      }
+    }
+
+    > a {
+      color: #ff9000;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 0.2s;
+
+      display: flex;
+      align-items: center;
+
+      svg {
+        margin-right: 16px;
+      }
+
+      &:hover {
+        color: ${shade(0.2, '#ff9000')};
+      }
+
+    }
+`
+
+const appearFromLeftContainer = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX( 100% )
+  }
+  to {
+    opacity: 1;
+    transform: translateX( 0% )
+  }
+`
+
 export const Background = styled.div`
   flex: 1;
   background: url(${signInbackgroundImg}) no-repeat center;
   background-size: cover;
+
+  animation: ${appearFromLeftContainer} 1.5s;
+
 `
--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,4 +1,4 @@
-import styled  from 'styled-components'
+import styled, { keyframes }  from 'styled-components'
 import { shade } from 'polished'
 
 import signUpbackgroundImg from '../../assets/sign-up-background.png'
@@ -8,6 +8,8 @@ export const Container = styled.div`
 
   display: flex;
   align-items: stretch;
+
+  overflow-x: hidden;
 `
 export const Content = styled.div`
   display: flex;
@@ -19,50 +21,89 @@ export const Content = styled.div`
   width: 100%;
   max-width: 700px;
 
-  h1 {
-    margin-bottom: 24px;
+`
+
+const appearFromRight = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX( 50px )
   }
-
-  form {
-    margin: 80px 0;
-    width: 340px;
-    text-align: center;
-  }
-
-  a {
-    color: #f4ede8;
-    display: block;
-    margin-top: 24px;
-    text-decoration: none;
-    transition: color 0.2s;
-
-    &:hover {
-      color: ${shade(0.2, '#f4ede8')};
-    }
-  }
-
-  > a {
-    color: #f4ede8;
-    display: block;
-    margin-top: 24px;
-    text-decoration: none;
-    transition: color 0.2s;
-
-    display: flex;
-    align-items: center;
-
-    svg {
-      margin-right: 16px;
-    }
-
-    &:hover {
-      color: ${shade(0.2, '#f4ede8')};
-    }
-
+  to {
+    opacity: 1;
+    transform: translateX( 0px )
   }
 `
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  place-content: center;
+
+  width: 100%;
+  max-width: 700px;
+
+  animation: ${appearFromRight} 1s;
+
+  h1 {
+      margin-bottom: 24px;
+    }
+
+    form {
+      margin: 80px 0;
+      width: 340px;
+      text-align: center;
+    }
+
+    a {
+      color: #f4ede8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#f4ede8')};
+      }
+    }
+
+    > a {
+      color: #ff9000;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 0.2s;
+
+      display: flex;
+      align-items: center;
+
+      svg {
+        margin-right: 16px;
+      }
+
+      &:hover {
+        color: ${shade(0.2, '#ff9000')};
+      }
+
+    }
+`
+
+const appearFromRightContainer = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX( -100% )
+  }
+  to {
+    opacity: 1;
+    transform: translateX( 0% )
+  }
+`
+
 export const Background = styled.div`
   flex: 1;
   background: url(${signUpbackgroundImg}) no-repeat center;
   background-size: cover;
+
+  animation: ${appearFromRightContainer} 1.5s;
 `
